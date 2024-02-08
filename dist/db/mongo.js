8e34d58371b54cace261501a4b8f7a551e88ebca
amirahmadi <ahmadi_soheyl@live.nl>
2024-02-08 20:14:56 +0100

    mongodb

--- dist/db/mongo.js
+++ dist/db/mongo.js
@@ -5,11 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const colors_1 = __importDefault(require("colors"));
-const isProd_1 = require("../utils/isProd");
 mongoose_1.default.set('strictQuery', true);
-const LOCAL_MONGO_DB = 'mongodb://127.0.0.1:27017/localApiAi';
-const AZURE_COSMOS_DB = 'mongodb://testgenerator-server:CTDzcIpWuy2eipa2ZSPTG1p9dErO6ReZuBUu4KBHphuy4pDRN8TmsUWnHqRbsgDtiXGedFlaVkkAACDbQOEzGQ==@testgenerator-server.mongo.cosmos.azure.com:10255/testgenerator-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@testgenerator-server@';
-const connectionString = isProd_1.isProduction ? AZURE_COSMOS_DB : LOCAL_MONGO_DB;
+const connectionString = 'mongodb+srv://root:root@cluster0.2v1s9.mongodb.net/ApiAi?retryWrites=true&w=majority';
 if (!connectionString) {
     console.error(colors_1.default.red.underline('No DB connection string found. Please set the DB. Process exiting...'));
     process.exit(1);
