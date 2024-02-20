7c1857781fde1b83154331c7fca6d6f027f872f0
amirahmadi <ahmadi_soheyl@live.nl>
2024-02-20 05:29:20 +0100

    changed jwt settings

--- dist/helpers/openApiUtils/generateImportStatement.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateImportStatement = void 0;
-const generateImportStatement = (serviceImports) => {
-    const imports = Array.from(serviceImports).join(",\n  ");
-    return `import {${imports}} from './services';`;
-};
-exports.generateImportStatement = generateImportStatement;
-//# sourceMappingURL=generateImportStatement.js.map
